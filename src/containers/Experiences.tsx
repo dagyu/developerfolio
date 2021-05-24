7b11fe8374215be33b373f6d0789e1783559fe31
dagyu <dagyu95dev@gmail.com>
2021-05-24 13:19:30 +0200

    Update text

--- src/containers/Experiences.tsx
+++ src/containers/Experiences.tsx
@@ -6,12 +6,18 @@ const experiences = [
     duration: "2019-2020",
     role: "Software Engineer/Full Stack Developer",
     typeOfWork: "Freelance",
-    desc: `I developed a mobile app with [tomgeek27](https://github.com/tomgeek27) and [Alice Paracolli](https://www.behance.net/aaliceparaf66a) for a client. 
-    The app monitors diaphragmatic breathing via the accelerometer sensor and it analyzes the obtained respiratory chart,
-    if you are curious see it on [Google Play](https://play.google.com/store/apps/details?id=com.ritmu.app) or on [App Store](https://apps.apple.com/us/app/id1514766559).
-    This was a really challenging experience because It was the first time I faced a real world case. 
-    In particular I designed the architecture, developed the whole back-end part, developed an 
-    algorithm that analyze the breath frequency using machine learning techniques and developed some front-end components.
+    desc: `
+I developed a mobile app with [tomgeek27](https://github.com/tomgeek27) and [Alice Paracolli](https://www.behance.net/aaliceparaf66a) for a client. 
+The app monitors diaphragmatic breathing via the accelerometer sensor and it analyzes the obtained respiratory chart,
+if you are curious see it on [Google Play](https://play.google.com/store/apps/details?id=com.ritmu.app) or on [App Store](https://apps.apple.com/us/app/id1514766559).
+This was a really challenging experience because It was the first time I faced a real world case. 
+
+In particular I:
+
+* designed the architecture
+* developed the whole back-end part, 
+* developed an algorithm that analyze the breath frequency using machine learning techniques 
+* developed some front-end components.
     `,
     logo: " https://www.ritm-u.com/wp-content/uploads/2020/02/cropped-cropped-New-logo-yu-tuberrr-4-2.png",
   },
