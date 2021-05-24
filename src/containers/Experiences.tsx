2e3f12c733e90c372fad790dfb4988b54b16dfcb
dagyu <dagyu95dev@gmail.com>
2021-05-24 10:52:34 +0200

    Add Alice Paracolli

--- src/containers/Experiences.tsx
+++ src/containers/Experiences.tsx
@@ -6,7 +6,7 @@ const experiences = [
     duration: "2019-2020",
     role: "Software Engineer/Full Stack Developer",
     typeOfWork: "freelance",
-    desc: `I developed a mobile app with [tomgeek27](https://github.com/tomgeek27) for a client. 
+    desc: `I developed a mobile app with [tomgeek27](https://github.com/tomgeek27) and [Alice Paracolli](https://www.behance.net/aaliceparaf66a) for a client. 
     This app monitors diaphragmatic breathing via the accelerometer sensor and analyzes the respiratory chart obtained,
     if you are curious see it on [Google Play](https://play.google.com/store/apps/details?id=com.ritmu.app) or on [App Store](https://apps.apple.com/us/app/id1514766559).
     This was a really challenging experience because It was the first time I faced a real world case. 
